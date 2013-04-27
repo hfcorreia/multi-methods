ed3d35267c8d0035c69c20498f040c34f50f0513
Hugo Correia <hfcorreia91@gmail.com>
2013-04-27 19:03:02 +0100

    add find-method and can-apply?

--- ros.rkt
+++ ros.rkt
@@ -2,19 +2,18 @@
 ;;; Representation of a generic function
 (struct generic-function (parameters methods)
   #:mutable
-  #:property prop:procedure   (lambda vars
-                                (let* ((instance (car vars))
-                                       (args (cdr vars))
-                                       (temp-method (car (generic-function-methods instance))))
-                                  (if (equal? (length (generic-function-parameters instance)) (length args))
-                                      (eval `(,(method-body temp-method) ,@args))
-                                      (error "Invalid number of args")))))
+  #:property prop:procedure (lambda vars
+                              (let* ((instance (car vars))
+                                     (args (cdr vars))
+                                     (temp-method (find-method (generic-function-methods instance) args)))
+                                (if (equal? (length (generic-function-parameters instance)) (length args))
+                                    (eval `(,(method-body temp-method) ,@args))
+                                    (error "Invalid number of args")))))
 
 ;;; Representation of a method
 (struct method (parameters body))
 
 ;;;; Main Syntax Rules
-
 ;;; Defines a new generic function
 (define-syntax-rule 
   (defgeneric name parameters)
@@ -22,11 +21,11 @@
 
 ;;; Defines a new generic method
 (define-syntax-rule
-  (defmethod method-name ( (name . predicate) ... ) body)
-  (make-gen-method method-name '( (name . predicate) ... ) 'body))
+  (defmethod method-name ( (arg . predicate) ... ) body)
+  (make-gen-method method-name '( (arg . predicate) ... ) 'body))
 
-(define (make-gen-method name parameteres body)
-  (set-generic-function-methods! name (update-gen-method name parameteres body)))
+(define (make-gen-method method-name parameteres body)
+  (set-generic-function-methods! method-name (update-gen-method method-name parameteres body)))
 
 (define (update-gen-method name parameters body)
   (append (generic-function-methods name) (list (make-method parameters body))))
@@ -40,8 +39,21 @@
       '()
       (cons (caar parameters) (make-args-list (cdr parameters)))))
 
-;;;; Test Examples
+(define (can-apply? parameters  args)
+  (define (can-apply-aux parameters args)
+    (cond ((or (null? parameters) (null? args)) #t)
+          ((not (apply (eval (cadar parameters)) (list (car args)))) #f) ;look?!?
+          (else (can-apply-aux (cdr parameters) (cdr args)))))
+  (if (not(equal? (length parameters) (length args)))
+      #f
+      (can-apply-aux parameters args)))
 
+(define (find-method methods args)
+  (cond ((null? methods) (error "Method missing for arguments" args))
+        ((can-apply? (method-parameters (car methods)) args) (car methods))
+        (else (find-method (cdr methods) args))))
+
+;;;; Test Examples
 ;;; Factorial example
 (defgeneric fact (n))
 
@@ -56,3 +68,10 @@
 
 (defmethod add ((x number?) (y number?))
   (+ x y))
+
+(defmethod add ((x string?) (y string?))
+  (string-append x y))
+
+;;; Test
+(define (test-can-apply) (can-apply? '((x number?) (y number?)) '("12" 2)))
+(define (test-find-method) (find-method (generic-function-methods add) '(1 1)))
